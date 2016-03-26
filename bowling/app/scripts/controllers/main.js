b6cbbccf4c77e18e08f8c48c79e8a2655add119e
christophe Gaon <christophe.gaon@gmail.com>
2016-03-26 11:27:39 +0100

    projet en tu karma

---
+++ bowling/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularProjApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularProjApp
+ */
+angular.module('angularProjApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
